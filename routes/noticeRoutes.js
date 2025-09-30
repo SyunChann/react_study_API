85d16776561a59e3a446196139ddc99b9540abc3
mia <Ruing.OqO@gmail.com>
2025-09-30 15:51:48 +0900

    공지라우터

--- routes/noticeRoutes.js
+++ routes/noticeRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { createNotice,getAllNotices,getNoticeById,updateNotice,deleteNotice} = require('../controllers/noticeController');
 
 router.post('/notice',createNotice);
-router.put('/notice/',getAllNotices)
+router.put('/notice',getAllNotices)
 router.get('/notice/:id',getNoticeById);
 router.patch('/notice/:id',updateNotice);
 router.delete('/notice/:id',deleteNotice);
