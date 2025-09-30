d4ef72fef3a8a23f7347b1d60710e3fd09255dc0
mia <Ruing.OqO@gmail.com>
2025-09-30 16:02:44 +0900

    noticeRoutes

--- routes/noticeRoutes.js
+++ routes/noticeRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { createNotice,getAllNotices,getNoticeById,updateNotice,deleteNotice} = require('../controllers/noticeController');
 
 router.post('/notice',createNotice);
-router.put('/notice',getAllNotices)
+router.get('/notice',getAllNotices)
 router.get('/notice/:id',getNoticeById);
 router.patch('/notice/:id',updateNotice);
 router.delete('/notice/:id',deleteNotice);
 
+module.exports = router;
